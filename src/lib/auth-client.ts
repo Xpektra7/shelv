7672b67f8a58a89b9fc83b0af778c96f5a8a7263
Xpektra7 <xpektra7@gmail.com>
2026-06-05 00:02:17 +0100

    fix: added vriable baseUrl

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,8 @@
 import { createAuthClient } from 'better-auth/react'
+
+const baseURL = import.meta.env.VITE_BETTER_AUTH_URL ?? window.location.origin
+
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: 'http://localhost:3000',
+  baseURL,
 })
