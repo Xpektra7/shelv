fc4c30f4a600ddb29bf4fb66a04bc0924aa52ae1
Xpektra7 <xpektra7@gmail.com>
2026-06-05 17:40:23 +0100

    omo

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,6 @@
 import { createAuthClient } from 'better-auth/react'
 
-const baseURL = import.meta.env.BETTER_AUTH_URL ?? window.location.origin
-
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL,
+  baseURL: 'https://shelv-dev.vercel.app/',
 })
