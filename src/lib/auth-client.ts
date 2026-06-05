79a10a53dd58f0754d3ca072640c20efad3269ba
Xpektra7 <xpektra7@gmail.com>
2026-06-05 17:29:16 +0100

    fix: changed baseUrl

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from 'better-auth/react'
 
-const baseURL = import.meta.env.VITE_BETTER_AUTH_URL ?? window.location.origin
+const baseURL = import.meta.env.BETTER_AUTH_URL ?? window.location.origin
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
